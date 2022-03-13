ca4fc6c4ad12495b6e11ba4d4ae9455a989561d4
MehdiSamid <geekmed17@123.com>
2022-03-13 16:07:49 +0000

    Test

--- src/gestionstock/GestionStock.java
+++ src/gestionstock/GestionStock.java
@@ -15,6 +15,8 @@ public class GestionStock {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hello World !");
     }
     
 }
